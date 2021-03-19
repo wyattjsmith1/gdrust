d5d5d5149c331374b5b00490e08837b898eb7da5
Wyatt Smith <taysoftware@gmail.com>
2021-03-18 19:51:48 -0700

    Reordered some content in the readme and added contibuting guidelines. (#6)

---
+++ CONTRIBUTING.md
@@ -0,0 +1,22 @@
+Contributions are always welcome! In general, there are a few ways to contribute:
+
+1. Filing bugs
+2. Making suggestions
+3. Create PRs
+
+### Filing Bugs
+If you find a bug, please let me know. I make bugs a priority. When filing an issue,
+please choose the appropriate issue template and include a sample as well as what you
+think the expected output should be. If the issue involves the editor, a screenshot of
+Godot would be great.
+
+### Making Suggestions
+This project will hopefully get better over time, and that starts with proposals. In 
+general, please ensure proposals follow this project's philosophy of improving 90% of
+use cases. We are trying to keep the syntax as close to GdScript as possible and are
+favoring readability over supporting 100% of cases.
+
+### Creating PRs
+PRs are always welcome. If you are making a PR to resolve a specific issue, please link 
+to that issue. If you are adding a new feature, please consider filing an issue before 
+you implement the change so it can be discussed by the community.
--- gdnative_helper/src/lib.rs
+++ gdnative_helper/src/lib.rs
@@ -7,92 +7,6 @@
 //! 1. A `gdrust!` macro for simplifying some rust code and making it more GdScript-like.
 //! 2. A set of "unsafe" functions to make things more concise at the risk of crashing.
 //!
-//! # Reasoning For This Project
-//! The team at [`gdnative-rust`](https://github.com/godot-rust/godot-rust) has done a wonderful job
-//! of making it possible to add Rust code to Godot. The main goal of their project is to enable
-//! developers to create safe, Rusty code for Godot, and that is now a reality.
-//!
-//! One of the shortcommings of `gdnative-rust` is that it is, well, very Rusty. Rust is a great
-//! language for many things, but the translation with Godot becomes a bit rocky sometimes. One such
-//! example is Rust's separation of data and logic through `struct`s and `impl`s. This undoubtedly
-//! leads to better code, but doesn't work too well with Godot, which uses `class`es because
-//! properties are part of the data (`struct`), but exposed in logic (`impls`).
-//!
-//! To surface an example: exporting properties. Currently, using `gdnative-rust` we must export
-//! properties like:
-//! ```ignore
-//! #[derive(gdnative::NativeClass)]
-//! #[inherit(MeshInstance)]
-//! #[register_with(register_properties)]
-//! struct RustTest {
-//!     test: u64,
-//! }
-//! fn register_properties(builder: &ClassBuilder<RustTest>) {
-//!     builder
-//!         .add_property::<String>("test/test_enum")
-//!         .with_hint(IntHint::Enum(EnumHint::new(vec![
-//!             "Hello".into(),
-//!             "World".into(),
-//!             "Testing".into(),
-//!         ])))
-//!         .with_getter(|this: &RustTest, _| this.test)
-//!         .with_setter(|this: &mut RustTest, _, value| this.test = value)
-//!         .done();
-//! }
-//! ```
-//! Users may optionally use the `#[property]` macro, but that prevents the user from creating
-//! signals, and exposes very little in the way of export hints.
-//!
-//! When you compare the block above to the equivalent GdScript, you see there is a huge difference:
-//! ```gdscript
-//! class_name RustTest
-//! extends MeshInstance
-//! @export_enum("Hello", "World", "Testing") var test
-//! ```
-//!
-//! To offer another example, let's look at signals:
-//! ```ignore
-//! #[derive(NativeClass)]
-//! #[inherit(Node)]
-//! #[register_with(Self::register_signals)]
-//! struct SignalEmitter {
-//!     timer: f64,
-//!     data: i64,
-//! }
-//! #[methods]
-//! impl SignalEmitter {
-//!     fn register_signals(builder: &ClassBuilder<Self>) {
-//!         builder.add_signal(Signal {
-//!             name: "tick_with_data",
-//!             args: &[SignalArgument {
-//!                 name: "data",
-//!                 default: Variant::from_i64(100),
-//!                 export_info: ExportInfo::new(VariantType::I64),
-//!                 usage: PropertyUsage::DEFAULT,
-//!             }],
-//!         });
-//!     }
-//! }
-//! ```
-//! And the equivalent GdScript:
-//! ```gdscript
-//! class_name SignalEmitter
-//! extends Node
-//! signal tick_with_data(data)
-//! ```
-//! Again, GdScript is substantially more concise. At the bottom of this page is a sample script
-//! written in GdRust that is about 50 lines. When expanded and formatted, it expands to over 750
-//! lines! That's almost 15x larger!!!
-//!
-//! So, why did `gdnative-rust` create this ridiculously verbose way of exporting properties? Well,
-//! they are just mirroring GdNative's property interface and keeping it Rusty. There is nothing
-//! wrong with this method (in fact, it gives developers full functionallity), but it can be
-//! overwhelming when compared to GdScript or most other Godot-supported languages.
-//!
-//! Some developers may be torn between the saftey of Rust and the speed and conciseness of
-//! GdScript. This library attempts to take the best of both Rust and GdScript to enable concise,
-//! fast, and safe code for Godot.
-//!
 //! # Goals
 //! Ultimately, the goal of this project is to make coding much easier in 90% of cases. There may
 //! be some edge cases only "true" rust can resolve, and this project should not comprimise its
@@ -312,6 +226,93 @@
 //! You should definitely read about the panics each method can produce and understand
 //! [`gdnative-rust`'s memory model](https://docs.rs/gdnative/0.9.3/gdnative/struct.Ref.html). Once
 //! you do, you should have the right judgement on when to use these helper functions.
+//!
+//!
+//! # Reasoning For This Project
+//! The team at [`gdnative-rust`](https://github.com/godot-rust/godot-rust) has done a wonderful job
+//! of making it possible to add Rust code to Godot. The main goal of their project is to enable
+//! developers to create safe, Rusty code for Godot, and that is now a reality.
+//!
+//! One of the shortcommings of `gdnative-rust` is that it is, well, very Rusty. Rust is a great
+//! language for many things, but the translation with Godot becomes a bit rocky sometimes. One such
+//! example is Rust's separation of data and logic through `struct`s and `impl`s. This undoubtedly
+//! leads to better code, but doesn't work too well with Godot, which uses `class`es because
+//! properties are part of the data (`struct`), but exposed in logic (`impls`).
+//!
+//! To surface an example: exporting properties. Currently, using `gdnative-rust` we must export
+//! properties like:
+//! ```ignore
+//! #[derive(gdnative::NativeClass)]
+//! #[inherit(MeshInstance)]
+//! #[register_with(register_properties)]
+//! struct RustTest {
+//!     test: u64,
+//! }
+//! fn register_properties(builder: &ClassBuilder<RustTest>) {
+//!     builder
+//!         .add_property::<String>("test/test_enum")
+//!         .with_hint(IntHint::Enum(EnumHint::new(vec![
+//!             "Hello".into(),
+//!             "World".into(),
+//!             "Testing".into(),
+//!         ])))
+//!         .with_getter(|this: &RustTest, _| this.test)
+//!         .with_setter(|this: &mut RustTest, _, value| this.test = value)
+//!         .done();
+//! }
+//! ```
+//! Users may optionally use the `#[property]` macro, but that prevents the user from creating
+//! signals, and exposes very little in the way of export hints.
+//!
+//! When you compare the block above to the equivalent GdScript, you see there is a huge difference:
+//! ```gdscript
+//! class_name RustTest
+//! extends MeshInstance
+//! @export_enum("Hello", "World", "Testing") var test
+//! ```
+//!
+//! To offer another example, let's look at signals:
+//! ```ignore
+//! #[derive(NativeClass)]
+//! #[inherit(Node)]
+//! #[register_with(Self::register_signals)]
+//! struct SignalEmitter {
+//!     timer: f64,
+//!     data: i64,
+//! }
+//! #[methods]
+//! impl SignalEmitter {
+//!     fn register_signals(builder: &ClassBuilder<Self>) {
+//!         builder.add_signal(Signal {
+//!             name: "tick_with_data",
+//!             args: &[SignalArgument {
+//!                 name: "data",
+//!                 default: Variant::from_i64(100),
+//!                 export_info: ExportInfo::new(VariantType::I64),
+//!                 usage: PropertyUsage::DEFAULT,
+//!             }],
+//!         });
+//!     }
+//! }
+//! ```
+//! And the equivalent GdScript:
+//! ```gdscript
+//! class_name SignalEmitter
+//! extends Node
+//! signal tick_with_data(data)
+//! ```
+//! Again, GdScript is substantially more concise. At the bottom of this page is a sample script
+//! written in GdRust that is about 50 lines. When expanded and formatted, it expands to over 750
+//! lines! That's almost 15x larger!!!
+//!
+//! So, why did `gdnative-rust` create this ridiculously verbose way of exporting properties? Well,
+//! they are just mirroring GdNative's property interface and keeping it Rusty. There is nothing
+//! wrong with this method (in fact, it gives developers full functionallity), but it can be
+//! overwhelming when compared to GdScript or most other Godot-supported languages.
+//!
+//! Some developers may be torn between the saftey of Rust and the speed and conciseness of
+//! GdScript. This library attempts to take the best of both Rust and GdScript to enable concise,
+//! fast, and safe code for Godot.
 //!
 //! # FAQ
 //! **Q**: I don't have experince in Rust; I just know GdScript. Can I just paste my scripts here for
